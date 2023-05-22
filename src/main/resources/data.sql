40f7131f51222235ab03c27d1757ec77d18534a7
eliseevm <wakuum1@yandex.ru>
2023-05-22 19:44:26 +0300

    Дополнить описание проекта инструкцией для запуска в докере, исправить данные тестового пользователя

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO captains_team (username, password, surname, email, birthday, medic_p
                            chronic_diseases, metal_implants, electronic_implants,
                            necessary_medicines, role, create_date)
 VALUES ('Максим', '$2a$10$5vgBWkhVMSpyTXJgXQStHecp1OhdCFYcrhO8IRG3Xp.GCqlZZe1qC', 'Елисеев', 'wakuum1@yandex.ru',
-        '1974-02-08', '123456789', '+79165935361', 'Москва', '+', '1', 'Аллергии нет', 'Противопоказаний нет', 'Хронических заболеваний нет',
+        '1947-02-08', '123456789', '+79161234567', 'Москва', '+', '1', 'Аллергии нет', 'Противопоказаний нет', 'Хронических заболеваний нет',
         'Металлических имплантов нет', 'Электронных имплантов нет', 'Препаратов принимаемых на постоянной основе нет','ROLE_USER', '2023-12-05');
